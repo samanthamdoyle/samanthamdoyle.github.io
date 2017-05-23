be9adb127b6f193616b3fdeeaed12b8e9afa14f4
samanthamdoyle <smdoy4@student.monash.edu>
2017-05-23 14:00:31 +1000

    hover states

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,97 @@
 $(document).ready(function() {
 
 
-// Add jQuery here
+  $(".appropriate").mouseenter(function() {
+      $(".appropriate__image").addClass("showme");
+    });
 
+    $(".appropriate").mouseleave(function() {
+      $(".appropriate__image").removeClass("showme");
+    });
+
+
+    $(".sam").mouseenter(function() {
+        $(".sam__image").addClass("showme");
+      });
+
+      $(".sam").mouseleave(function() {
+        $(".sam__image").removeClass("showme");
+      });
+
+
+      $(".tings").mouseenter(function() {
+          $(".tings__image").addClass("showme");
+        });
+
+        $(".tings").mouseleave(function() {
+          $(".tings__image").removeClass("showme");
+        });
+
+
+        $(".identity").mouseenter(function() {
+            $(".identity__image").addClass("showme");
+          });
+
+          $(".identity").mouseleave(function() {
+            $(".identity__image").removeClass("showme");
+          });
+
+
+          $(".mad").mouseenter(function() {
+              $(".mad__image").addClass("showme");
+            });
+
+            $(".mad").mouseleave(function() {
+              $(".mad__image").removeClass("showme");
+            });
+
+            $(".pool").mouseenter(function() {
+                $(".pool__image").addClass("showme");
+              });
+
+              $(".pool").mouseleave(function() {
+                $(".pool__image").removeClass("showme");
+              });
+
+              $(".wild").mouseenter(function() {
+                  $(".wild__image").addClass("showme");
+                });
+
+                $(".wild").mouseleave(function() {
+                  $(".wild__image").removeClass("showme");
+                });
+
+                $(".nsfw").mouseenter(function() {
+                    $(".nsfw__image").addClass("showme");
+                  });
+
+                  $(".nsfw").mouseleave(function() {
+                    $(".nsfw__image").removeClass("showme");
+                  });
+
+                  $(".card").mouseenter(function() {
+                      $(".card__image").addClass("showme");
+                    });
+
+                    $(".card").mouseleave(function() {
+                      $(".card__image").removeClass("showme");
+                    });
+
+                    $(".boots").mouseenter(function() {
+                        $(".boots__image").addClass("showme");
+                      });
+
+                      $(".boots").mouseleave(function() {
+                        $(".boots__image").removeClass("showme");
+                      });
+
+                      $(".wtf").mouseenter(function() {
+                          $(".wtf__image").addClass("showme");
+                        });
+
+                        $(".wtf").mouseleave(function() {
+                          $(".wtf__image").removeClass("showme");
+                        });
 
 
 
