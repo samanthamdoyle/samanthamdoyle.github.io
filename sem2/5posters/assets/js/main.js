7a591ea7798c80b8b3363bb750918a7534a2ad06
samanthamdoyle <smdoy4@student.monash.edu>
2017-08-16 20:36:03 +1000

    posters

--- sem2/5posters/assets/js/main.js
+++ sem2/5posters/assets/js/main.js
@@ -1,27 +1,103 @@
-$(document).ready(function() {
+ jQuery(document).ready(function(){
 
 
-// Add jQuery here
+   $('div').hide();
 
+    setTimeout(function(){
+    $('div.1a').fadeIn('slow');},100);
 
+    setTimeout(function(){
+    $('div.1b').fadeIn('slow');},200);
 
+    setTimeout(function(){
+    $('div.1c').fadeIn('slow');},300);
 
+    setTimeout(function(){
+    $('div.1d').fadeIn('slow');},400);
 
+    setTimeout(function(){
+    $('div.1e').fadeIn('slow');},500);
 
+    setTimeout(function(){
+    $('div.1f').fadeIn('slow');},600);
 
+    setTimeout(function(){
+    $('div.1g').fadeIn('slow');},700);
 
+    setTimeout(function(){
+    $('div.1h').fadeIn('slow');},800);
 
+    setTimeout(function(){
+    $('div.1i').fadeIn('slow');},900);
 
+    setTimeout(function(){
+    $('div.2a').fadeIn('slow');},1000);
 
+    setTimeout(function(){
+    $('div.2b').fadeIn('slow');},1100);
 
+    setTimeout(function(){
+    $('div.2c').fadeIn('slow');},1200);
 
+    setTimeout(function(){
+    $('div.2d').fadeIn('slow');},1300);
 
+    setTimeout(function(){
+    $('div.2e').fadeIn('slow');},1400);
 
+    setTimeout(function(){
+    $('div.2f').fadeIn('slow');},1500);
 
+    setTimeout(function(){
+    $('div.2g').fadeIn('slow');},1600);
 
+    setTimeout(function(){
+    $('div.2h').fadeIn('slow');},1700);
 
+    setTimeout(function(){
+    $('div.3a').fadeIn('slow');},1800);
 
+    setTimeout(function(){
+    $('div.3b').fadeIn('slow');},1900);
+
+    setTimeout(function(){
+    $('div.3c').fadeIn('slow');},2000);
+
+    setTimeout(function(){
+    $('div.3d').fadeIn('slow');},2100);
+
+    setTimeout(function(){
+    $('div.4a').fadeIn('slow');},2200);
+
+    setTimeout(function(){
+    $('div.4b').fadeIn('slow');},2300);
+
+    setTimeout(function(){
+    $('div.4c').fadeIn('slow');},2400);
+
+    setTimeout(function(){
+    $('div.4d').fadeIn('slow');},2500);
+
+    setTimeout(function(){
+    $('div.4e').fadeIn('slow');},2600);
+
+    setTimeout(function(){
+    $('div.5a').fadeIn('slow');},2700);
+
+    setTimeout(function(){
+    $('div.5b').fadeIn('slow');},2800);
+
+    setTimeout(function(){
+    $('div.5c').fadeIn('slow');},2900);
+
+    setTimeout(function(){
+    $('div.5d').fadeIn('slow');},3000);
+
+    setTimeout(function () {
+      $('div').addClass('addred');}, 3300);
 
 
 
   });
+
+// Add jQuery here
